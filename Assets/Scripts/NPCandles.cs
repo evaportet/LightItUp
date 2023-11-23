caf8fd1d98acdf10c47aa690e666954d0b199631
PauAmigóArjona <pau.amigo@enti.cat>
2023-11-23 13:17:50 +0100

    LevelChange

--- Assets/Scripts/NPCandles.cs
+++ Assets/Scripts/NPCandles.cs
@@ -1,24 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NPCandles : MonoBehaviour
 {
     public bool isLit;
     CharacterController charC;
     [SerializeField] GameObject fire;
+    [SerializeField] string[] scenes;
+    [SerializeField] float levelChangeDelay;
+    [SerializeField] int currentScene;
+    int maxScene;
 
     private void Start()
     {
         if (isLit)
             fire.SetActive(true);
-        else 
+        else
             fire.SetActive(false);
     }
     public void LightUp()
     {
         isLit = true;
         fire.SetActive(true);
+        StartCoroutine("LevelChange");
+    }
+
+    IEnumerator LevelChange()
+    {
+        yield return new WaitForSeconds(levelChangeDelay);
+        if (currentScene == scenes.Length - 1)
+            currentScene = 0;
+        else
+            currentScene++;
+        SceneManager.LoadScene(scenes[currentScene]);
     }
 }
